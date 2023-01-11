916d09b5d6714458f18d8f314ac6c406428424cd
Crimson-Ice <drujon.antonin@gmail.com>
2023-01-11 23:57:42 +0100

    carré 2

--- src/algorith/Main.java
+++ src/algorith/Main.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 public class Main {
 
-    public static Logger logger = Logger.getLogger(Main.class.getName());
+    public static final Logger logger = Logger.getLogger(Main.class.getName());
     public static void main(String[] args)
     {
         double res = polonaiseInverse(saisie());
